e858769c736e84512d7c660293c0ad7d8d280fc3
pratheeshrussell1992 <62546459+pratheeshrussell1992@users.noreply.github.com>
2021-05-26 09:41:13 +0530

    sql db-set storage to innodb

--- generators/app/templates/db/mysql/database.py
+++ generators/app/templates/db/mysql/database.py
@@ -1,13 +1,14 @@
 database_setting = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+		'STORAGE_ENGINE': 'InnoDB',
         'NAME': 'djangodb',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET storage_engine=InnoDB;SET sql_mode='STRICT_TRANS_TABLES'",
         }
     }
 }
